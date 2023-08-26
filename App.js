7431a2c65bcc0c63d3b1bf0373d00560f32c6d01
yash gupta <14yashapps@gmail.com>
2023-08-27 00:04:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom/client"
+
+
+const heading = (
+<h1>
+hii 
+
+</h1>
+
+
+) ;
+
+const root = ReactDom.createRoot(document.getElementById("root"))
+root.render(heading);
