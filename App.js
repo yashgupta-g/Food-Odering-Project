d514e78eefd19c3f852eace9667a60b98cdad264
yash gupta <14yashapps@gmail.com>
2023-08-31 22:30:36 +0530

    maps added

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom/client"
 import Header from "./src/Header";
-import Body from "./src/Body";
-import Footer from "./src/Footer";
+import {Body} from "./src/Body";
 
 
 const heading = (
 <>
 <Header/>
 <Body/>
-{/* <Footer/> */}
 </>
 
 
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Header.css"
 export default function Header() {
   return (
-    <div className='nav'>
+    <div className='nav '>
 <img src="https://i.ibb.co/st53b55/eatupp.png"  alt='eatupp'        />
 <ul>
 
